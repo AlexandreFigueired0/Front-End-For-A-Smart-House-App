0a0dc7f9ff7c97cb57cbe9f3660b234353a06444
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2022-04-28 21:55:14 +0100

    criação de um ficheiro de scripts que seja usados em varios ficheiros

--- scripts/garage_script.js
+++ scripts/garage_script.js
@@ -1,25 +1,6 @@
 const washCarBtn = document.getElementById("wash-car-btn")
-const temperatureBtns = Array.from(document.getElementsByClassName("temp-controller"))
-let temperatureValue = document.getElementById("temperature-value")
 
-temperatureBtns.forEach(b => b.addEventListener("click", () => changeTemperature(b)) )
 washCarBtn.addEventListener("click", startWashingSession)
-
-function changeTemperature(b){
-    let signal = b.textContent
-    let currentTemperature = parseInt(temperatureValue.textContent)
-    console.log(signal)
-
-    if(signal == "+" && currentTemperature<35){
-        currentTemperature++
-    }
-    else if(signal == "-" && currentTemperature>10){
-        currentTemperature--
-    }
-
-    temperatureValue.textContent = currentTemperature
-}
-
 
 function startWashingSession(){
     let warningWindow = document.getElementById("warning-window")
---
+++ scripts/scripts.js
@@ -0,0 +1,58 @@
+const temperatureBtns = Array.from(document.getElementsByClassName("temp-controller"))
+let temperatureValue = document.getElementById("temperature-value")
+let i = 0
+let width = 1;
+
+temperatureBtns.forEach(b => b.addEventListener("click", () => changeTemperature(b)) )
+
+
+function showLoading() {
+    var elem = document.getElementById("loading-bar");
+    elem.setAttribute('style', 'display:flex');
+    move();
+}
+
+function move() {
+    if(i == 0) {
+        i = 1;
+        var elem = document.getElementById("progress-bar");
+        var id = setInterval(frame, 30);
+        function frame() {
+            if(width >= 100) {
+                elem.innerHTML = "Refeição pronta a servir!"
+                playRefeicao();
+                clearInterval(id);
+                i = 0;
+            }
+            else {
+                width++;
+                elem.style.width = width + "%";
+                elem.innerHTML = width + "%";
+            }
+        }
+    }
+}
+
+function okOrCancelMeal() {
+    if(width == 100) {
+        var elem = document.getElementById("loading-bar");
+        elem.setAttribute('style', 'display:none');
+        width = 0;
+    }
+}
+
+
+function changeTemperature(b){
+    let signal = b.textContent
+    let currentTemperature = parseInt(temperatureValue.textContent)
+    console.log(signal)
+
+    if(signal == "+" && currentTemperature<35){
+        currentTemperature++
+    }
+    else if(signal == "-" && currentTemperature>10){
+        currentTemperature--
+    }
+
+    temperatureValue.textContent = currentTemperature
+}
