093bc3c3c8a9819af74c0254939c24c7d31ee20c
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2022-04-28 23:22:15 +0100

    botoes da barra ativam e desativam bem

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,15 +13,20 @@ function showLoading() {
 }
 
 function move() {
+    let ok = document.getElementById("OK")
+    let cancel = document.getElementById("cancel")
     if(i == 0) {
         i = 1;
         var elem = document.getElementById("progress-bar");
         var id = setInterval(frame, 30);
         function frame() {
             if(width >= 100) {
-                elem.innerHTML = "Processo Concluído!"
+                elem.innerHTML = "Tarefa Concluída"
                 clearInterval(id);
                 i = 0;
+                ok.classList = ""
+                ok.addEventListener("click",() => location='wc.html')
+                cancel.classList.add("disabled")
             }
             else {
                 width++;
