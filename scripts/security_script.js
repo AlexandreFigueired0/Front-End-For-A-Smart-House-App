62134ff29b0a59b96dec1e4bc1c9cec6dea6865d
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2022-04-29 22:53:11 +0100

    correçao da refeicao, da avaliacao heursitica

--- scripts/security_script.js
+++ scripts/security_script.js
@@ -1,3 +1,9 @@
 const alertImg = document.getElementById("alert")
+console.log(alertImg)
 
 alertImg.addEventListener("click", showIntrusorAlert)
+
+function showIntrusorAlert(){
+    let window = document.getElementById("alert-window")
+    window.style.display = "block"
+}
