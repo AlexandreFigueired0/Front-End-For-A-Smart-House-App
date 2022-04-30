e95947032da68c11a7265ca4f71198c08612a3ac
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2022-04-30 15:37:45 +0100

    schedule guarda tempo! schedule terminado

--- scripts/schedule_script.js
+++ scripts/schedule_script.js
@@ -1,7 +1,49 @@
-const DAY_INDEX = 3
-const labels = document.querySelectorAll("input[type = checkbox]")
+// localStorage.clear()
 
-labels.forEach(cb => cb.addEventListener("click", () => showConfig(cb)))
+const DAY_INDEX = 3
+const CHECKBOX_STATES = "scheduleCheckBoxStates"
+const TIME_STATES = "scheduleTimeStates"
+let checkBoxes = Array.from(document.querySelectorAll("input[type = checkbox]"))
+let checkBoxesState = [false,false,false,false,false,false,false]
+let storedStates = localStorage.getItem(CHECKBOX_STATES)
+let timeInputs = Array.from(document.querySelectorAll("input[type = time]"))
+let timeStates = ["","","","","","",""]
+let storedTimes = localStorage.getItem(TIME_STATES)
+
+if(storedTimes){
+    timeStates = JSON.parse(storedTimes)
+    for(let i = 0 ; i<timeInputs.length; i++){
+        timeInputs[i].value = timeStates[i]
+    }
+}
+
+if(storedStates){
+    checkBoxesState = JSON.parse(storedStates)
+    for(let i = 0; i<checkBoxesState.length;i++){
+        checkBoxes[i].checked = checkBoxesState[i]
+        showConfig(checkBoxes[i])
+    }
+}
+
+checkBoxes.forEach(c => c.addEventListener("click",() => clickCheckBox(c)))
+timeInputs.forEach(t => t.addEventListener("focusout",()=> saveTime(t)))
+
+function saveTime(t){
+    let index = timeInputs.indexOf(t)
+    timeStates[index] = t.value
+    localStorage.setItem(TIME_STATES,JSON.stringify(timeStates))
+
+}
+
+function clickCheckBox(c){
+    showConfig(c)
+    saveStates()
+}
+
+function saveStates(){
+    checkBoxes.forEach(c => checkBoxesState[checkBoxes.indexOf(c)] = c.checked)
+    localStorage.setItem(CHECKBOX_STATES, JSON.stringify(checkBoxesState))
+}
 
 function showConfig(cb){
     let divElem = cb.parentElement.parentElement
