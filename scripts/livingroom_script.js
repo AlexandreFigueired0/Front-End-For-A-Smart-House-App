d4bb4db7cb5c51af490d8afdd83c6b228ebd387a
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2022-05-01 11:48:07 +0100

    Sala ja tudo funciona e guarda em localStorage

--- scripts/livingroom_script.js
+++ scripts/livingroom_script.js
@@ -1,15 +1,84 @@
 const MAX_TEMPERATURE = 35
 const MIN_TEMPERATURE = 10
 const TEMPERATURE_KEY = "livingRoomTemperature"
+const LIGHTS_KEY = "livingRoomLightsValue"
+const BLINDS_KEY = "livingRoomBlindsValue"
+const TV_KEY = "livingRoomTvValue"
+const PROJECTOR_KEY = "livingRoomProjectorValue"
+
 let temperatureBtns = Array.from(document.getElementsByClassName("temp-controller"))
 let temperatureValue = document.getElementById("temperature-value")
 let storedTemperature = localStorage.getItem(TEMPERATURE_KEY)
+let lightsCheckBox = document.querySelector("#lights-btn input")
+let storedLightValue = localStorage.getItem(LIGHTS_KEY)
+let blindsBtn = document.querySelector("#blinds-btn button")
+let storedBlindsBtnValue = localStorage.getItem(BLINDS_KEY)
+let tvBtn = document.getElementById("tVOnOff")
+let storedTvValue = localStorage.getItem(TV_KEY)
+let projectorBtn = document.getElementById("projetorOnOff")
+let storedProjectorValue = localStorage.getItem(PROJECTOR_KEY)
+
+if(storedProjectorValue){
+    projectorBtn.textContent = storedProjectorValue
+}
+
+if(storedTvValue){
+    tvBtn.textContent = storedTvValue
+}
+
+if(storedBlindsBtnValue){
+    blindsBtn.textContent = storedBlindsBtnValue
+}
 
 if(storedTemperature){
     temperatureValue.textContent = storedTemperature
 }
 
+lightsCheckBox.checked = storedLightValue == "true"
+
+projectorBtn.addEventListener("click", saveProjectorButtonText)
+tvBtn.addEventListener("click", saveTvButtonText)
+blindsBtn.addEventListener("click", saveBlindsButtonText )
+lightsCheckBox.addEventListener("click", saveCheckBoxValue)
 temperatureBtns.forEach(b => b.addEventListener("click", () => changeTemperature(b)) )
+
+function saveProjectorButtonText(){
+    let value = projectorBtn.textContent
+    if(value == "Ligar"){
+        projectorBtn.textContent = "Desligar"
+    }
+    else{
+        projectorBtn.textContent = "Ligar"
+    }
+    localStorage.setItem(PROJECTOR_KEY, projectorBtn.textContent)
+}
+
+function saveTvButtonText(){
+    let value = tvBtn.textContent
+    if(value == "Ligar"){
+        tvBtn.textContent = "Desligar"
+    }
+    else{
+        tvBtn.textContent = "Ligar"
+    }
+    localStorage.setItem(TV_KEY, tvBtn.textContent)
+}
+
+function saveBlindsButtonText(){
+    let value = blindsBtn.textContent
+    if(value == "Abrir"){
+        blindsBtn.textContent = "Fechar"
+    }
+    else{
+        blindsBtn.textContent = "Abrir"
+    }
+    localStorage.setItem(BLINDS_KEY, blindsBtn.textContent)
+}
+
+function saveCheckBoxValue(){
+    let value = lightsCheckBox.checked
+    localStorage.setItem(LIGHTS_KEY, value ? "true" : "false")
+}
 
 function changeTemperature(b){
     let signal = b.textContent
