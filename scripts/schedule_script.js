c022bce30e51d62f6c0cb52f98bff626a241c638
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2022-05-04 20:16:29 +0100

    CONSEGUI FAZER O HORARIO

--- scripts/schedule_script.js
+++ scripts/schedule_script.js
@@ -1,9 +1,21 @@
+// localStorage.clear()
 const CHECKBOX_STATES = "scheduleCheckBoxStates"
+const COUNTER_KEY = "counter"
+const LIST_KEY = "scheduleList"
+
+let count = 3
+let storedCount = localStorage.getItem(COUNTER_KEY)
 
 let list = document.getElementById("schedule-list")
+let storedList = localStorage.getItem(LIST_KEY)
+if(storedList){
+    list.innerHTML = storedList
+}
 let listElems = Array.from(document.querySelectorAll("#schedule-list li"))
 
-let checkBoxes = Array.from(document.querySelectorAll("#schedule-list input"))
+let checkBoxes = Array.from(list.querySelectorAll("#schedule-list input"))
+console.log(checkBoxes)
+console.log(listElems)
 let checkBoxStates = []
 for(let i = 0; i<listElems.length ; i++){
     checkBoxStates.push(false)
@@ -11,6 +23,14 @@ for(let i = 0; i<listElems.length ; i++){
 let storedCheckBoxStates = localStorage.getItem(CHECKBOX_STATES)
 
 let addBtn = document.getElementById("add")
+
+let configWindow = document.getElementById("config-window")
+
+
+
+if(storedCount){
+    count = parseInt(storedCount)
+}
 
 if(storedCheckBoxStates){
     checkBoxStates = JSON.parse(storedCheckBoxStates)
@@ -23,8 +43,86 @@ addBtn.addEventListener("click", addNewSchedule)
 checkBoxes.forEach(c => c.addEventListener("click", () => clickCheckBox(c)))
 
 
-function addNewSchedule(){
+async function addNewSchedule(){
     showConfigsWindow();
+    let okBtn = document.getElementById("ok")
+    okBtn.addEventListener("click",() => saveConfigs(okBtn))
+}
+
+
+
+function saveConfigs(b){
+    if(isSubmitable()){
+        count++
+        localStorage.setItem(COUNTER_KEY,count)
+        let days = Array.from(configWindow.querySelectorAll(".day"))
+
+
+        let hours = configWindow.querySelector("#hour")
+        let mins = configWindow.querySelector("#min")
+        let duration = configWindow.querySelector("#dur")
+        let timeUnit = configWindow.querySelector("#time-unit")
+        let listItem = document.createElement("li")
+        let timeSpan = document.createElement("span")
+        let daysSpan = document.createElement("span")
+        let switchLabel = document.createElement("label")
+        let cb = document.createElement("input")
+        let sliderSpan = document.createElement("span")
+
+        timeSpan.id = JSON.stringify(count).concat("schedule")
+        timeSpan.classList.add("time")
+        timeSpan.append((hours.value.length == 1 ? "0" + hours.value  : hours.value)+ ":" +( mins.value.length == 1 ? "0" + mins.value : mins.value))
+
+        let daysStr = checkedDays(days)
+        daysSpan.classList.add("days")
+        daysSpan.append("Dias: " + daysStr)
+
+        switchLabel.classList.add("switch")
+
+        cb.type = "checkbox"
+
+        sliderSpan.classList.add("slider")
+        sliderSpan.classList.add("round")
+
+        switchLabel.appendChild(cb)
+        switchLabel.appendChild(sliderSpan)
+        timeSpan.appendChild(daysSpan)
+        listItem.appendChild(timeSpan)
+        listItem.appendChild(switchLabel)
+
+        list.appendChild(listItem)
+        listElems.push(listItem)
+        localStorage.setItem(LIST_KEY,list.innerHTML)
+
+        configWindow.style.display = "none"
+    }
+    else{
+        window.alert("Preencha os campos todos")
+    }
+}
+
+
+function checkedDays(l){
+    let res = []
+    for(let i = 0; i<l.length; i++){
+        if(l[i].checked){
+            res.push(l[i].parentElement.textContent)
+        }
+    }
+    return res.join(" ")
+}
+
+function isSubmitable(){
+    let valid = false
+    let checkDays = Array.from(configWindow.querySelectorAll("input[type = checkbox]"))
+    for(let i = 0 ; i<checkDays.length ; i++){
+        valid = valid || checkDays[i].checked
+    }
+    let hours = configWindow.querySelector("#hour")
+    let mins = configWindow.querySelector("#min")
+    let duration = configWindow.querySelector("#dur")
+    valid = valid && hours.value && mins.value && duration.value
+    return valid
 }
 
 function clickCheckBox(c){
@@ -34,6 +132,5 @@ function clickCheckBox(c){
 
 
 function showConfigsWindow(){
-    let configWindow = document.getElementById("config-window")
     configWindow.style.display = "block"
 }
